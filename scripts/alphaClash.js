cbe4d33c1b8c80d8e1151c97279d3ccfc1d1ac6e
oworkmahade <wtbl.hhasan@gmailcom>
2024-08-26 22:50:33 +0600

    commited

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -1,21 +1,47 @@
+// game start
 function play() {
+  // playground screen show
+  showElementById("playground-section");
   // home screen hidden
   hideElementById("home-section");
+  hideElementById("score");
+  // game started
+  continueGame();
 
-  //   playground screen show
-  showElementById("playground-section");
-
-  //   game started
-  startGame();
+  // ......................................
+  // reset life value to 5
+  setUpdatedValueById("life-value", 5);
+  // reset score value 0
+  setUpdatedValueById("score-value", 0);
 }
 
-function startGame() {
+// game continue
+function continueGame() {
   // step-1: generate a random alphabet
-  const alphabet = getRandomAlphabet();
+  const alphabet = createRandomAlphabet();
 
   //   step-1: display random number on screen
   displayRandomAlphabet(alphabet);
 
   //   step-2: set background color to displayed alphabet
   setBackgroundColor(alphabet);
+}
+
+// game over
+function gameOver() {
+  // playground layout hide
+  // reusable function
+  hideElementById("playground-section");
+
+  // final score layout display
+  // reusable function
+  showElementById("score");
+
+  // display final score
+  const score = document.getElementById("score-value").innerText;
+  setUpdatedValueById("final-score", score);
+
+  // clear the last selected alphabet
+  const selectedAlphabet = getTextElementValueById("display-alphabet");
+  removeBackgroundColor(selectedAlphabet);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -10,8 +10,8 @@ function showElementById(elementId) {
   element.classList.remove("hidden");
 }
 
-// get random alphabet
-function getRandomAlphabet() {
+// create random alphabet
+function createRandomAlphabet() {
   // create an alphabet array
   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
   const alphabets = alphabetString.split("");
@@ -39,19 +39,76 @@ function setBackgroundColor(elementId) {
   alphabetColor.classList.add("bg-key");
 }
 
+// key press and magic
 // addEventListener for capture the pressed key and expected key
 // [call back function], when add  one function inline with addEventListener then it is called callback function.
 
 // document.addEventListener("keyup", pressedKey);
 document.addEventListener("keyup", function (event) {
-  pressedKey(event); // must pass argument at times of using parameter in function
-  expectedKey();
+  const pressedKeyVal = pressedKey(event); // must pass argument at times of using parameter in function
+  const expectedKeyVal = expectedKey();
+
+  console.log(pressedKeyVal);
+
+  // ESC key press for game over
+  if (pressedKeyVal === "Escape") {
+    gameOver();
+  }
+  // score
+  if (pressedKeyVal === expectedKeyVal) {
+    // start a new round
+    continueGame();
+    removeBackgroundColor(pressedKeyVal);
+
+    // update score
+    //.......................................................................
+    // 1.get the current score
+    // const elementId = document.getElementById("score-value");
+    // const currentScore = parseInt(elementId.innerText);
+
+    // 2.increase the score by 1
+    // const updatedScore = currentScore + 1;
+
+    // 3.display the updated score
+    // elementId.innerText = updatedScore;
+    //........................................................................
+
+    // ** using reusable function
+    const currentScore = getCurrentValueById("score-value");
+    const updatedScore = currentScore + 1;
+    setUpdatedValueById("score-value", updatedScore);
+  }
+  // life
+  else {
+    // update life
+    //........................................................................
+    // 1.get the current life
+    // const elementId = document.getElementById("life-value");
+    // const currentLife = parseInt(elementId.innerText);
+    // 2.reduce the life by 1
+    // const updatedLife = currentLife - 1;
+    // 3.display the updated life
+    // elementId.innerText = updatedLife;
+    //........................................................................
+
+    // ** using reusable function
+    const currentLife = getCurrentValueById("life-value");
+    const updatedLife = currentLife - 1;
+    setUpdatedValueById("life-value", updatedLife);
+
+    // game exit
+    // final score
+    // condition for game end
+    if (updatedLife === 0) {
+      gameOver();
+    }
+  }
 });
 
 // get pressed key
 function pressedKey(event) {
   const pressedKey = event.key;
-  console.log("player pressed:", pressedKey);
+  return pressedKey;
 }
 
 // get expected key
@@ -60,5 +117,30 @@ function expectedKey() {
   const expectedKey = document
     .getElementById("display-alphabet")
     .innerText.toLowerCase();
-  console.log("expected key:", expectedKey);
+  return expectedKey;
+}
+
+// remove alphabet background color
+function removeBackgroundColor(elementId) {
+  const alphabetColor = document.getElementById(elementId);
+  alphabetColor.classList.remove("bg-key");
+}
+
+// * reusable function of getting life - score current value
+function getCurrentValueById(elementId) {
+  const currentValue = parseInt(document.getElementById(elementId).innerText);
+  return currentValue;
+}
+
+// * reusable function of displaying life - score updated value
+function setUpdatedValueById(id, value) {
+  const elementId = document.getElementById(id);
+  elementId.innerText = value;
+}
+
+// get text element from artboard
+// note: here we may use getCurrentValueById() but for parseInt function it is not possible
+function getTextElementValueById(elementId) {
+  const element = document.getElementById(elementId).innerText;
+  return element;
 }
